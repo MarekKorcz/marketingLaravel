6bc6a7c3e5e93dd05ea19c1d468ef10efe5bc668
MarkKorcz <mark.korcz@gmail.com>
2021-04-13 13:07:10 +0200

    new contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,27 +9,42 @@
     <div class="row">
       <div class="col-12">
           <div class="contact-section-header">
-              <p>SKONTAKTUJ SIĘ Z NAMI</p>
-              <p style="font-size: 1.5rem;">(+48) 602 342 396</p>
-              <p style="font-size: 1.2rem;">Lub napisz na:</p>
-              <hr>
-              <div class="row">
-                  <div class="col-4"></div>
-                  <div class="col-2">
-                      <p>
-                          <a id="instagram" class="nav-link" href="https://www.facebook.com/Mark-Media-927015074159735" target="_blank">
-                              <i class="fa fa-facebook" style="font-size: 51px;"></i>
-                          </a>
-                      </p>
-                  </div>
-                  <div class="col-2">
-                      <p>
-                          <a id="instagram" class="nav-link" href="https://www.instagram.com/mark.media.group/" target="_blank">
-                              <i class="fa fa-instagram" style="font-size: 51px; color: orange;"></i>
-                          </a>
-                      </p>
-                  </div>
-                  <div class="col-4"></div>
+            <figure class="quote">
+                <blockquote>
+                    "Rzeczy mogą przyjść do tych, którzy czekają, lecz będą to tylko rzeczy pozostawione przez tych, którzy postanowili być kowalami własnego losu."
+                </blockquote>
+                <figcaption>
+                    &mdash; Abraham Lincoln 
+                </figcaption>
+            </figure>
+            <p>
+                Pomyśl o swojej konkurencji i o tym co się z nią stanie kiedy żyły Twójego biznesu wypełnią się życiodajnymi zleceniami od 
+                <strong>
+                    Twoich
+                </strong>
+                nowych klientów! 
+            </p>
+            <p style="font-size: 1.8rem;">Następnie skontaktuj się z nami</p>
+            <p style="font-size: 1.5rem;">(+48) 602 342 396</p>
+            <p style="font-size: 1.2rem;">Lub napisz na:</p>
+            <hr>
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-2">
+                    <p>
+                        <a id="instagram" class="nav-link" href="https://www.facebook.com/Mark-Media-927015074159735" target="_blank">
+                            <i class="fa fa-facebook" style="font-size: 51px;"></i>
+                        </a>
+                    </p>
+                </div>
+                <div class="col-2">
+                    <p>
+                        <a id="instagram" class="nav-link" href="https://www.instagram.com/mark.media.group/" target="_blank">
+                            <i class="fa fa-instagram" style="font-size: 51px; color: orange;"></i>
+                        </a>
+                    </p>
+                </div>
+                <div class="col-4"></div>
               </div>
               <hr>
           </div>
